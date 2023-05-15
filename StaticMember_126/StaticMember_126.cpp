fd379c6811f2286cba1c47125830e8e17fa5a377
mutiara126 <salsamutiara1812@gmail.com>
2023-05-15 17:56:15 +0800

    untuk menyimpan judul

--- StaticMember_126/StaticMember_126.cpp
+++ StaticMember_126/StaticMember_126.cpp
@@ -10,10 +10,10 @@ public:
 
 	void setID();
 	void printAll();
-	mahasiwa(string pnama) :nama(pnama){ setID();}
+	mahasiswa(string pnama) :nama(pnama){ setID();}
 };
 
-int mahasiswa :: nim = 0);
+int mahasiswa :: nim = 0;
 
 void mahasiswa::setID() {
 	id = ++nim;
@@ -23,8 +23,19 @@ void mahasiswa::printAll(){
 	cout << "ID = "<<id<< endl;
 	cout << "Nama = " << nama<<endl;
 	cout << endl;
+}
 
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kuniawan");
+	mahasiswa mhs4("Joko Purbo");
 
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
 
 
