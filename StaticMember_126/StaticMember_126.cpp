045c85456617d00787d515877e2fcfc539f88244
mutiara126 <salsamutiara1812@gmail.com>
2023-05-15 12:40:02 +0800

    membuat void mahasiswa printAll

--- StaticMember_126/StaticMember_126.cpp
+++ StaticMember_126/StaticMember_126.cpp
@@ -10,13 +10,21 @@ public:
 
 	void setID();
 	void printAll();
-	mahasiwa(string pnama) :nama(pnama) { setID(); }
+	mahasiwa(string pnama) :nama(pnama){ setID();}
 };
 
 int mahasiswa :: nim = 0);
 
 void mahasiswa::setID() {
 	id = ++nim;
+}
+
+void mahasiswa::printAll(){
+	cout << "ID = "<<id<< endl;
+	cout << "Nama = " << nama<<endl;
+	cout << endl;
+
+
 }
 
 
