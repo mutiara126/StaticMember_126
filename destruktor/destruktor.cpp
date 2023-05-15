4b6037d0df701c5e0618b9c8d02bf482824d0482
mutiara126 <salsamutiara1812@gmail.com>
2023-05-15 19:14:41 +0800

    untuk membuat metode cetak

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -10,10 +10,41 @@ public:
 	~angka();     //Destruction
 	void cetakData();
 	void isiData();
-
 };
+//Definisi member function
+angka::angka(int i) { //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData;
+}
 
+angka::~angka() { //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat array sudah dilepaskan" << endl;
+}
 
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+    }
+}
+
+void angka::cetakData() {
+	for (int i = 1;i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constructor dipanggil
+	delete ptrBelajarcpp; //Deastructor dipanggil
+
+	return 0;
+}//Destructor dipanggil
 
 
 
---
+++ konstruktor/konstruktor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+mahasiswa();
+mahasiswa(int);
+mahasiswa(int nim, string nim);
+void cetak();
+
+
+};
